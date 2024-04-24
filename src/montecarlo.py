b6ba890e14da204f505a98ee26b6e8cdcaa4a90f
Ken <73114932+kennardmah@users.noreply.github.com>
2024-04-24 13:24:50 +0100

    Energy Demand

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -1,27 +1,48 @@
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
-def generate_demand_scenarios(annual_growth, initial_demand, std_dev, num_years, num_scenarios):
-    """
-    Generate energy demand scenarios using Monte Carlo simulation.
+# Number of simulations
+n_simulations = 10000
 
-    :param annual_growth: Annual growth rate of demand (percentage)
-    :param initial_demand: Initial energy demand at the starting period
-    :param std_dev: Standard deviation of demand growth
-    :param num_years: Number of years for which to forecast demand
-    :param num_scenarios: Number of scenarios to simulate
-    :return: DataFrame of demand scenarios
-    """
-    years = np.arange(num_years)
-    scenarios = np.zeros((num_years, num_scenarios))
-    
-    for i in range(num_scenarios):
-        growth_rates = np.random.normal(annual_growth, std_dev, num_years)
-        scenarios[:, i] = initial_demand * np.cumprod(1 + growth_rates)
-    
-    return pd.DataFrame(scenarios, index=years, columns=[f'Scenario_{i+1}' for i in range(num_scenarios)])
+def P_ECLSS(occupants, energy_per_occupants):
+    return (occupants * energy_per_occupants), 0.1*(occupants * energy_per_occupants)
 
-# Example usage
-if __name__ == "__main__":
-    demand_scenarios = generate_demand_scenarios(0.02, 100, 0.01, 10, 1000)
-    print(demand_scenarios.head())
+def P_operations(operations):
+    total_energy = 0
+    for op, energy in operations:
+        total_energy += energy
+    return total_energy, 0.1*total_energy
+
+# Phase 1 parameters
+ECLSS_p1 = [6, 4.2]
+mean_ECLSS_p1, std_dev_ECLSS_p1 = P_ECLSS(ECLSS_p1[0], ECLSS_p1[1])  # kW
+
+OPERATIONS_p1 = [('ISRU Operations', 7), ('Rover Operations', 1), ('Some other stuff', 1)]
+mean_operations_p1, std_dev_operations_p1 = P_operations(OPERATIONS_p1)
+
+# Phase 2 parameters
+ECLSS_p2 = [12, 4.2]
+mean_ECLSS_p2, std_dev_ECLSS_p2 = P_ECLSS(ECLSS_p2[0], ECLSS_p2[1])  # kW
+
+OPERATIONS_p2 = [('ISRU Operations', 14), ('Rover Operations', 2), ('Some other stuff', 2)]
+mean_operations_p2, std_dev_operations_p2 = P_operations(OPERATIONS_p2)
+
+# Simulate Phase 1
+ECLSS_p1 = np.random.normal(mean_ECLSS_p1, std_dev_ECLSS_p1, n_simulations)
+operations_p1 = np.random.normal(mean_operations_p1, std_dev_operations_p1, n_simulations)
+total_demand_p1 = ECLSS_p1 + operations_p1
+
+# Simulate Phase 2
+ECLSS_p2 = np.random.normal(mean_ECLSS_p2, std_dev_ECLSS_p2, n_simulations)
+operations_p2 = np.random.normal(mean_operations_p2, std_dev_operations_p2, n_simulations)
+total_demand_p2 = ECLSS_p2 + operations_p2
+
+# Plot results
+plt.figure(figsize=(14, 6))
+plt.hist(total_demand_p1, bins=50, alpha=0.5, label='Phase 1 Demand')
+plt.hist(total_demand_p2, bins=50, alpha=0.5, label='Phase 2 Demand')
+plt.title('Monte Carlo Simulation of Energy Demand on the Moon')
+plt.xlabel('Total Energy Demand (kW)')
+plt.ylabel('Frequency')
+plt.legend()
+plt.show()
