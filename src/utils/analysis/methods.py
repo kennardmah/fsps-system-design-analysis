2ef895af7d1b5ca4c40e39ae78b0559abfec190b
Ken <73114932+kennardmah@users.noreply.github.com>
2024-05-07 16:58:24 +0100

    update

---
+++ src/utils/analysis/methods.py
@@ -0,0 +1,32 @@
+import csv
+
+"""
+Manually include arrays for each flexible and inflexible methods
+"""
+
+# Implementation methods
+inflexible_50 = ["inflexible_50", 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+inflexible_60 = ["inflexible_60", 60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+flexible_30_0 = ["flexible_30_0", 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+flexible_30_10 = ["flexible_30_10", 30, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+flexible_30_20 = ["flexible_30_20", 30, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+flexible_40_0 = ["flexible_40_0", 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+flexible_40_10 = ["flexible_40_10", 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+flexible_40_20 = ["flexible_40_20", 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+data = [
+    inflexible_50,
+    inflexible_60,
+    flexible_30_0,
+    flexible_30_10,
+    flexible_30_20,
+    flexible_40_0,
+    flexible_40_10,
+    flexible_40_20
+]
+
+filename = "/Users/kennardmah/Documents/GitHub/masters-thesis-sustainable-lunar-energy/src/utils/data/implementation_methods.csv"
+
+with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(data)
--- src/utils/models/costModel.py
+++ src/utils/models/costModel.py
@@ -1,5 +1,8 @@
-### this file needs to evaluate the continuous cost of C_penalty * shortfall at each time step
 import numpy as np
+
+"""
+costModel.py – calculate the cost at each time step and return the total cost
+"""
 
 # Penalty Integral Calculation
 def calculate_C_penalty(capacity, demand, t, alpha_penalty = 5000000):
@@ -7,15 +10,24 @@ def calculate_C_penalty(capacity, demand, t, alpha_penalty = 5000000):
     return alpha_penalty * integral
 
 # Development Cost
-def calculate_C_capital(M_nf, M_components, C_launch, C_produce, C_payload, r_eos = 0):
+def calculate_C_capital(kW, mass_ratio = 3969/40, C_launch = 2720, C_produce = 43000, C_payload = 3500, r_eos = 0):
+    M_nf = kW * mass_ratio
+    M_components = 1621/40 * kW
     M_total = M_nf + M_components
     C_nf = (M_total) * C_launch + (M_nf * C_produce + (M_total) * C_payload) * (1 - r_eos)
     return C_nf
 
 # Operational Cost at t
-def calculate_C_operational(M_nf, M_components, cost_per_mass, t):
+def calculate_C_operational(M_nf, M_components, cost_per_mass = 0):
     return (M_nf + M_components) * cost_per_mass
 
 # Total Cost at t
-def calculate_C_total(C_capital, C_operational, C_penalty):
+def calculate_C_total(kW, t):
+    C_capital = calculate_C_capital(kW_added)
+    C_operational = calculate_C_operational(3969/40 * kW, 1621/40 * kW)
+    C_penalty = calculate_C_penalty(kW_produced, t)
     return C_capital + C_operational + C_penalty
+
+def add_C_capital(cost_overtime, kW, t):
+    cost_overtime[t] += calculate_C_capital(kW)
+    return cost_overtime
