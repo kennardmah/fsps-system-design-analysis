1c00192eb8c7d4c69cbd061550c237b9b0bf6f19
Ken <73114932+kennardmah@users.noreply.github.com>
2024-05-16 18:08:13 +0100

    May 16

--- src/utils/analysis/testing/alpha_penalty.py
+++ src/utils/analysis/testing/alpha_penalty.py
@@ -9,9 +9,19 @@ import costModel as cm
 sys.path.append('src/utils/analysis')
 import expectedPayoff as ep
 import cumulativeDistribution as cd
+import numpy as np
 
+def find_intersections(x_values, y_values):
+    for i in range(1, len(y_values)):
+        y1, y2 = y_values[i-1], y_values[i]
+        if y1 * y2 <= 0:  # where it changes sign
+            x1, x2 = x_values[i-1], x_values[i]
+            # Linear interpolation formula to find the x-value where y = 0
+            return(x1 - y1 * (x2 - x1) / (y2 - y1))
+            
 alpha_penalty = [500000]
-alpha_penalty = [5000 * i for i in range(0, 400)]
+alpha_penalty = [54750 * i for i in range(0, 33)]
+print(alpha_penalty)
 graph = []
 for alpha in alpha_penalty:
     cm.main(alpha)
@@ -40,7 +50,6 @@ plotting methods
 # ax.set_ylabel('$ per kW missed')
 # ax.legend()
 # plt.show()
-
 x_results, y_results = [], []
 vert_line = 0
 for alpha_val, values in graph:
@@ -49,9 +58,20 @@ for alpha_val, values in graph:
     y_results.append(best_inflex - best_flex)
     if best_inflex - best_flex > 0: vert_line = alpha_val/(20*365*24)
 
+# Convert y_results to a numpy array
+y_results = np.array(y_results)
+
 # Create the plot
-plt.plot(x_results, y_results, color = '0.3', linestyle='-')
+plt.plot(x_results, y_results, color='0.3', linestyle='-')
+plt.fill_between(x_results, y_results, 0, hatch = '/', edgecolor = '0.5', facecolor='0.8')
 plt.xlabel('Penalty for Energy Shortage ($/kWh)')
-plt.ylabel('E[LCOE] Benefit of Flexibility over Inflexibility ($/kWh)')
-plt.axvline(x=vert_line, color='0.5', linestyle='--')
+plt.ylabel('Benefit of Flexibility over Inflexibility ( ΔE[LCOE] )')
+plt.axvline(x=(find_intersections(x_results, y_results)), color='0.5', linestyle='--')
+plt.axhline(y=0, color='0.3', linestyle='-')
+plt.grid(True)
+plt.tight_layout() 
+plt.ylim(y_results[-1], y_results[0])
+plt.xlim(0, 10)
+plt.xticks([i for i in range(0, 11)])
+plt.yticks([i for i in range(int(y_results[-1]-1), int(y_results[0])+2)])
 plt.show()
