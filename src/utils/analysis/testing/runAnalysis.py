eacc9b8dfe35c0951284dcee92518c4ce7151640
Ken <73114932+kennardmah@users.noreply.github.com>
2024-05-31 16:42:33 +0100

    May 31: update

--- src/utils/analysis/testing/runAnalysis.py
+++ src/utils/analysis/testing/runAnalysis.py
@@ -9,8 +9,8 @@ import cumulativeDistribution as cd
 # intersection for E[LCOE]          
 alpha_penalty = 5 * 20 * 365 * 24
 
-def main_tree(alpha=5*20*365*24, simulation=False, plot=True, choose_best=True):
-    cm.main(alpha=alpha, simulation=simulation)
+def main_tree(alpha=5*20*365*24, simulation=False, plot=True, choose_best=True, discount=0.05):
+    cm.main(alpha=alpha, simulation=simulation, discount=discount)
     ep.main()
     cd.main_tree(plot=plot, choose_best=choose_best)
 
@@ -20,5 +20,5 @@ def main_sim(alpha=5 * 20 * 365 * 24,simulation=True,plot=True):
     cd.main_sim(plot=plot)
 
 if __name__ == "__main__":
-    main_tree(alpha=5*20*365*24, simulation=True, plot=True, choose_best=False)
+    main_tree(alpha=5*20*365*24, simulation=True, plot=True, choose_best=False, discount=0.05)
     main_sim(alpha=alpha_penalty)
